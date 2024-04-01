eb62991252eada758e1bf40a2b05a4baa7cecf2e
Sh6abrA <andpro22223@gmail.com>
2024-04-02 01:56:56 +0500

    Add react and style loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,26 @@
 import webpack from 'webpack';
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+    const typeScriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    const cssLoaders = {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+    }
+    //Если не используем TS, то нужен babel-loader
     return [
-        {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        },
+        typeScriptLoader,
+        cssLoaders
     ]
 }
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-function someFunction(arg: number): string {
-    console.log(arg);
-    return arg.toString();
-}
-export default someFunction;
