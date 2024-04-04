caa6e5a0026f7f0cdf3f5876792bbc024cd097cc
Sh6abrA <andpro22223@gmail.com>
2024-04-04 16:41:04 +0500

    Add lang support

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,8 @@
 import './styles/index.scss'
 import { AppRouter } from 'app/providers/router';
 import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme';
+import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
@@ -11,13 +13,13 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-
-            <Navbar />
-            <div className="content-page">
-                <Sidebar />
-                <AppRouter />
-            </div>
-
+            <Suspense fallback=''>
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 }
---
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -0,0 +1,29 @@
+
+import { useTranslation } from "react-i18next";
+
+import { classNames } from 'shared/lib/classNames/classNames';
+import classes from './LangSwitcher.module.scss';
+import { Button, ThemeButton } from "shared/ui/Button/Button";
+
+interface LangSwitcherProps {
+    className?: string
+}
+
+export const LangSwitcher = ({ className }: LangSwitcherProps) => {
+    const { t, i18n } = useTranslation();
+
+    const toggle = () => {
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+    }
+
+    return (
+        <Button
+            theme={ThemeButton.CLEAR}
+            onClick={toggle}
+            className={classNames(classes.LangSwitcher, {}, [className])}
+        >
+            {t('Язык')}
+        </Button>
+    );
+}
+
