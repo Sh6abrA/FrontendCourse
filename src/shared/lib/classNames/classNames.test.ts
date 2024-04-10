53f28574d341ae450d6d9a569d4104979c416b72
Sh6abrA <andpro22223@gmail.com>
2024-04-10 19:03:19 +0500

    Add jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,18 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+    test('with additional class', () => {
+        const expected = 'someClass pdg someClass2';
+        expect(classNames('someClass', {}, ['pdg', 'someClass2'])).toBe(expected);
+    });
+    test('with mods', () => {
+        const expected = 'someClass pdg someClass2 hovered scrolled';
+        expect(classNames('someClass',
+            { hovered: true, selectable: false, scrolled: true },
+            ['pdg', 'someClass2']))
+            .toBe(expected);
+    })
+})
