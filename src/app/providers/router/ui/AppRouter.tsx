3f9d27b0605eb3d88f2cd6ed55470e66508561ba
Sh6abrA <andpro22223@gmail.com>
2024-04-10 20:47:33 +0500

    Add Loader and Norfound page

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -2,10 +2,11 @@ import { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Suspense } from "react";
 import { routeConfig } from "shared/config/routeConfig/routeConfig";
+import { PageLoader } from "widgets/PageLoader";
 
 const AppRouter: FC = () => {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={ <PageLoader/> }>
             <Routes>
                 {Object.values(routeConfig).map(({ element, path }) => <Route key={path} path={path} element={(
                     <div className="page-wrapper">
