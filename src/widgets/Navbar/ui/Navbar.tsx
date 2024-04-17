9c2f79a71e84ef359c2672b59463fbff37308aa3
Sh6abrA <andpro22223@gmail.com>
2024-04-17 22:55:07 +0500

    Correct lint

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable i18next/no-literal-string */
 import { classNames } from 'shared/lib/classNames/classNames';
 import classes from './Navbar.module.scss';
-import { AppLink } from 'shared/ui/AppLink/AppLink';
-import { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+
 
 
 
