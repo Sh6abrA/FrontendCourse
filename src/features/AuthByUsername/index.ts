4cbe5e434ef6b95b5928c09e623167fc00af6993
Sh6abrA <andpro22223@gmail.com>
2024-04-21 01:36:18 +0500

    add reducer for login

--- src/features/AuthByUsername/index.ts
+++ src/features/AuthByUsername/index.ts
@@ -1,2 +1,8 @@
-import { LoginModal } from 'features/AuthByUsername/ui/LoginModal/LoginModal';
-export {LoginModal};
+import { LoginModal } from './ui/LoginModal/LoginModal';
+import { LoginSchema } from './model/types/loginSchema';
+import { loginReducer } from './model/slice/loginSlice';
+export { 
+    LoginModal, 
+    LoginSchema,
+    loginReducer
+} 
--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,25 +1,51 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './LoginForm.module.scss';
 import { useTranslation } from 'react-i18next';
-import { Button } from 'shared/ui/Button/Button';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { Input } from 'shared/ui/Input/Input';
+import { useDispatch, useSelector } from 'react-redux';
+import { memo, useCallback } from 'react';
+import { loginActions } from '../../model/slice/loginSlice';
+import { getLoginState } from '../../model/selectors/getLoginState/getLoginState';
 
 interface LoginFormProps {
     className?: string
 }
 
-export const LoginForm = ({ className }: LoginFormProps) => {
+export const LoginForm = memo(function LoginForm({ className }: LoginFormProps) {
     const { t } = useTranslation();
+    const dispatch = useDispatch();
+    const loginForm = useSelector(getLoginState);
 
+
+    const onChangeUsername = useCallback((value: string) => {
+        dispatch(loginActions.setUsername(value))
+    }, [dispatch]);
+    const onChangePassword = useCallback((value: string) => {
+        dispatch(loginActions.setPassword(value))
+    }, [dispatch]);
 
     return (
         <div className={classNames(cls.LoginForm, {}, [className])}>
-            <Input autofocus placeholder={t('Введите username')}type="text" className={cls.input}/>
-            <Input placeholder={t('Введите password')} type="text" className={cls.input}/>
-            <Button className={cls.loginBtn}>
+            <Input
+                onChange={onChangeUsername}
+                autofocus
+                placeholder={t('Введите username')}
+                type="text"
+                className={cls.input}
+                value={loginForm.username}
+            />
+            <Input
+                onChange={onChangePassword}
+                placeholder={t('Введите password')}
+                type="text"
+                className={cls.input}
+                value={loginForm.password}
+            />
+            <Button theme={ButtonTheme.OUTLINE} className={cls.loginBtn}>
                 {t('Войти')}
             </Button>
         </div>
     );
-}
+})
 
